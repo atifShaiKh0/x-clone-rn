e1210b0a07637e8a13fa14c8770a2a56124ffe4b
atifShaikh0 <maatif881@gamil.com>
2025-09-06 07:24:19 +0530

    All User Routes Done

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { 
+    followUser, 
+    getCurrentUser, 
+    getUserProfile, 
+    syncUser, 
+    updateProfile 
+} from "../controllers/user.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/profile/:username", getUserProfile);
+
+
+// protected route
+
+router.post("/sync", protectRoute, syncUser);
+
+router.post("/me", protectRoute, getCurrentUser);
+
+router.put("/profile", protectRoute, updateProfile);
+
+router.post("/follow/:targetUserId", protectRoute, followUser);
+
+
+
+//update profile => authicanticated
+
+
+export default router;
